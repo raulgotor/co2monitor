10548245b18cb1e8b0da5ad767ce8e005519e10c
Raúl Gotor <raul.gotor@midge-medical.com>
2022-05-29 16:50:47 +0200

    updated documentation

--- main/sensor.c
+++ main/sensor.c
@@ -68,10 +68,13 @@
  *******************************************************************************
  */
 
+//! @brief Sensor task
 _Noreturn static void sensor_task(void *pvParameter);
 
+//! @brief Module mutex lock
 static inline bool sensor_lock(bool lock);
 
+//! @brief UART transfer function for esp32
 static mh_z19_error_t xfer_func(uint8_t const * const p_rx_buffer,
                                 size_t const rx_buffer_size,
                                 uint8_t * const p_tx_buffer,
@@ -83,10 +86,13 @@ static mh_z19_error_t xfer_func(uint8_t const * const p_rx_buffer,
  *******************************************************************************
  */
 
+//! @brief Handler for the module's task
 TaskHandle_t sensor_task_h = NULL;
 
+//! @brief Handler for the display module queue
 extern xQueueHandle display_q;
 
+//! @brief Handler for the http module queue
 extern xQueueHandle http_q;
 
 /*
@@ -95,19 +101,32 @@ extern xQueueHandle http_q;
  *******************************************************************************
  */
 
+//! @brief UART instance used by this module
 static uart_port_t const m_uart_instance = UART_NUM_2;
 
+//! @brief UART TX gpio used by this module
 static int const m_uart_tx_pin = 33;
 
+//! @brief UART RX gpio used by this module
 static int const m_uart_rx_pin = 32;
 
+//! @brief Handle for the UART mutex used by this module
 static QueueHandle_t m_uart_mutex_q = NULL;
+
 /*
  *******************************************************************************
  * Public Function Bodies                                                      *
  *******************************************************************************
  */
 
+/*!
+ * @brief Initialize the sensor module
+ *
+ * This function initializes the needed hardware peripherals (UART), the sensor
+ * driver and the corresponding module task
+ *
+ * @return              bool                Operation result
+ */
 bool sensor_init(void) {
 
         uart_config_t const uart_config = {
@@ -197,6 +216,16 @@ bool sensor_init(void) {
  *******************************************************************************
  */
 
+/*!
+ * @brief Module mutex lock
+ *
+ * Used to lock whatever needed by this module (basically, UART access)
+ *
+ * @param[in]           lock                Whether it needs to be locked our
+ *                                          unlocked
+ *
+ * @return              bool                Operation result
+ */
 static inline bool sensor_lock(bool lock)
 {
         BaseType_t semaphore_result;
@@ -210,7 +239,29 @@ static inline bool sensor_lock(bool lock)
         return (pdTRUE == semaphore_result);
 }
 
-static mh_z19_error_t xfer_func(uint8_t const * const p_rx_buffer,
+/*!
+ * @brief UART transfer function for esp32
+ *
+ * Translation unit between the application code and the hardware framework
+ *
+ * @note `p_rx_buffer` and `p_tx_buffer` cannot be both null
+ *
+ * @param[out]          p_rx_buffer         Pointer to the buffer where to read
+ *                                          the data to
+ * @param[in]           rx_buffer_size      Size of the read buffer
+ * @param[in]           p_tx_buffer         Pointer to the buffer where to read
+ *                                          the data from
+ * @param[in]           tx_buffer_size      Size of the write buffer
+ *
+ * @return              mh_z19_error_t      Operation result
+ * @retval              MH_Z19_ERROR_SUCCESS
+ *                                          Everything went well
+ * @retval              MH_Z19_ERROR_BAD_PARAMETER
+ *                                          Parameter is null
+ * @retval              MH_Z19_ERROR_IO_ERROR
+ *                                          Error while doing a transfer operation
+ */
+ static mh_z19_error_t xfer_func(uint8_t const * const p_rx_buffer,
                                 size_t const rx_buffer_size,
                                 uint8_t * const p_tx_buffer,
                                 size_t const tx_buffer_size)
@@ -260,6 +311,23 @@ static mh_z19_error_t xfer_func(uint8_t const * const p_rx_buffer,
  *******************************************************************************
  */
 
+/*!
+ * @brief Sensor task
+ *
+ * This task will wait for `http_q` and `display_q` to be ready before entering
+ * in an endless loop. The loop will wait for any notifications, and the loop rate
+ * will be controlled by the timeout of the `xTaskNotifyWait`function.
+ *
+ * The actions this module will perform are:
+ * - Post sensor data to HTTP module (if wifi connection)
+ * - Post sensor data to display module (if display is on)
+ * - Calibrate sensor (if calibrate button was pressed)
+ *
+ *
+ * @param               pvParameter         Not used
+ *
+ * @return              -                   -
+ */
 _Noreturn static void sensor_task(void * pvParameter) {
 
         uint32_t co2_ppm;
